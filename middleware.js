bf2a446dcf19264985cfc3b006c39be88517f60a
PillageDev <pillageyt@gmail.com>
2024-06-30 17:35:14 -0400

    Middleware now works to check if they are logged in

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { checkJWT } from "@/lib/api";
+import {NextResponse} from "next/server";
+
+export async function middleware(req, res, next) {
+    if (req.nextUrl.pathname.startsWith("/_next") || /\.[^/]*$/.test(req.nextUrl.pathname)) {
+        return NextResponse.next()
+    }
+
+    const path = req.url;
+    console.log(path);
+
+    if (path.includes("/login") || path.includes("/signup")) {
+        return
+    }
+
+    let token = req.cookies.token;
+    if (!token) {
+        return NextResponse.redirect(new URL("/login", req.url))
+    }
+
+    const valid = await checkJWT(token);
+    if (!valid) {
+        return NextResponse.redirect(new URL("/login", req.url))
+    }
+}
